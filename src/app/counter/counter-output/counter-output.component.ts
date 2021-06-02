738067571b6caac044c8fdec697975a44a3f1e6d
Mandeepsahoo <mandeepsahoo97@gmail.com>
2021-06-03 00:08:21 +0530

    custom input  action

---
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
+import { CounterState } from '../state/counter.state';
+
+@Component({
+  selector: 'app-counter-output',
+  templateUrl: './counter-output.component.html',
+  styleUrls: ['./counter-output.component.css'],
+})
+export class CounterOutputComponent implements OnInit {
+  // @Input() counter: number;
+  // counter: number;
+  // counterSubscription: Subscription;
+
+  counter$: Observable<{ counter: number }>;
+
+  constructor(private store: Store<{ counter: CounterState }>) {}
+
+  ngOnInit(): void {
+    // this.counterSubscription = this.store
+    //   .select('counter')
+    //   .subscribe((data) => {
+    //     this.counter = data.counter;
+    //   });
+    this.counter$ = this.store.select('counter');
+  }
+
+  // ngOnDestroy() {
+  //   if (this.counterSubscription) {
+  //     this.counterSubscription.unsubscribe();
+  //   }
+  // }
+}
