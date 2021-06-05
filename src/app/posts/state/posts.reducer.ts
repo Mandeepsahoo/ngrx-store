cd5d833f4087ff6c4d19964176744d9446b8ad2f
Mandeepsahoo <mandeepsahoo97@gmail.com>
2021-06-05 12:52:09 +0530

    ngrx crud operations

--- src/app/posts/state/posts.reducer.ts
+++ src/app/posts/state/posts.reducer.ts
@@ -1,18 +1,38 @@
 import { createReducer, on } from '@ngrx/store';
-import { addPost } from './posts.action';
+import { addPost, deletePost, updatePost } from './posts.action';
 import { initialState } from './posts.state';
 
 const _postsReducer = createReducer(
   initialState,
   on(addPost, (state, action) => {
-    console.log(action, state);
-
     let post = { ...action.post };
     post.id = (state.posts.length + 1).toString();
 
     return {
       ...state,
       posts: [...state.posts, post],
+    };
+  }),
+  on(updatePost, (state, action) => {
+    const updatedPost = state.posts.map((post) => {
+      return action.post.id === post.id ? action.post : post;
+    });
+    return {
+      ...state,
+      posts: updatedPost,
+    };
+  }),
+  on(deletePost, (state, action) => {
+    console.log('action', action);
+    console.log('state', state);
+
+    const updatedPost = state.posts.filter((post) => {
+      return post.id !== action.id;
+    });
+
+    return {
+      ...state,
+      posts: updatedPost,
     };
   })
 );
